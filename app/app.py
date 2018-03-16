a2f27f8071fbec077445f87ed59ea1e1deb9bd1a
Rafael Cirolini <rafaelcirolini@not32540.corp.terra>
2018-03-16 10:07:25 -0300

    add hostname

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
+import socket
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    html = "<h3>Hello World!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" 
+
+    return html.format(hostname=socket.gethostname())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
