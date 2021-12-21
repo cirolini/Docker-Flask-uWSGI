5bd3c8aac153887c3488870b65b7fc882d01a510
Rafael Cirolini <rafael.cirolini@azion.com>
2021-12-21 10:59:27 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h3>Hello Peiter!</h3>" \
+    html = "<h3>Hello Fullstack!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" 
 
     return html.format(hostname=socket.gethostname())
